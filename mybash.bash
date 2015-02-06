ac173027b785f0f6cb6d35c2270f9940a2833d99
richardhethu <r4he@ucsd.edu>
2015-02-05 20:22:50 -0800

    Added bash script.

---
+++ mybash.bash
@@ -0,0 +1,16 @@
+res=1
+
+for (( k=5; k<=30; k+=5 ))
+do
+	for (( k2=5; k2<=20; k2+=5 ))
+	do
+		for eta in 0.001,0.01,0.1,1,10
+		do
+			for lambda in 0.01 0.1 1.0 10.0 100
+			do
+				./train reviews_Street\,_Surf_\&_Skate.votes.gz duplicate_list.txt.gz duplicate_image_list.txt.gz image_features_Street_Surf_Skate.b also_viewed.txt.gz $k $k2 $eta $lambda 400 1> imagemodel.res${res}.txt 2>&1
+				let res=${res}+1
+			done
+		done
+	done
+done
